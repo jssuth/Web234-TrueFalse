f90beb20f3aec1cfdb0b6463464fc928ac3a77c7
jssuth <77399142+jssuth@users.noreply.github.com>
2025-11-27 13:24:44 -0600

    Trigger rebuild

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/final-exam/',   //  MATCH EXACT REPO NAME
+  base: '/Web234/',   //  MATCH EXACT REPO NAME
 })
