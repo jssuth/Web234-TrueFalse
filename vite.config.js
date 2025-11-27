06a80843cc68f5e1225e59d794a2b3d081840510
jssuth <77399142+jssuth@users.noreply.github.com>
2025-11-27 13:02:43 -0600

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Web234/',   //  MATCH EXACT REPO NAME
+  base: '/final-exam/',   //  MATCH EXACT REPO NAME
 })
